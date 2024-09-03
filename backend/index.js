4d990e28d0ddd80167dfd2b4f54757c70c350277
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-09-02 23:36:57 -0400

    cambios

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const { pool, createTable, insertPost } = require('./db/config');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 
 // Crea la tabla al iniciar el servidor
